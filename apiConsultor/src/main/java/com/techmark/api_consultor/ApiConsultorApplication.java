6920088662b83242456c318c2b71613118cfe200
Joao <joaowictortessmann97@gmail.com>
2025-09-29 19:43:24 -0300

    Criação do Projeto da Atividade de Spring Boot

---
+++ apiConsultor/src/main/java/com/techmark/api_consultor/ApiConsultorApplication.java
@@ -0,0 +1,13 @@
+package com.techmark.api_consultor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiConsultorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiConsultorApplication.class, args);
+	}
+
+}
---
+++ apiConsultor/src/test/java/com/techmark/api_consultor/ApiConsultorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techmark.api_consultor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiConsultorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
